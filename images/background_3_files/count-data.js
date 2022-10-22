919d63b7a5d87cbf11fe093c14dbb2a750efacd1
MyGitHuby88 <116377342+MyGitHuby88@users.noreply.github.com>
2022-10-22 05:45:38 +0200

    Add files via upload

---
+++ images/background_3_files/count-data.js
@@ -0,0 +1,5 @@
+var DISQUSWIDGETS;
+
+if (typeof DISQUSWIDGETS != 'undefined') {
+    DISQUSWIDGETS.displayCount({"text":{"and":"and","comments":{"zero":"0 Comments","multiple":"{num} Comments","one":"1 Comment"}},"counts":[{"id":"http:\/\/www.allmofeed.com\/2014\/10\/css.html","comments":0}]});
+}
