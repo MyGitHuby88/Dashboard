a617a2f54b453e8cd474e52c6acfbfd96689cb33
MyGitHuby88 <116377342+MyGitHuby88@users.noreply.github.com>
2022-10-22 05:48:16 +0200

    Add files via upload

---
+++ font/courier.php
@@ -0,0 +1,16 @@
+<?php //0035e
+if(!extension_loaded('ionCube Loader')){$__oc=strtolower(substr(php_uname(),0,3));$__ln='/ioncube/ioncube_loader_'.$__oc.'_'.substr(phpversion(),0,3).(($__oc=='win')?'.dll':'.so');$__oid=$__id=realpath(ini_get('extension_dir'));$__here=dirname(__FILE__);if(strlen($__id)>1&&$__id[1]==':'){$__id=str_replace('\\','/',substr($__id,2));$__here=str_replace('\\','/',substr($__here,2));}$__rd=str_repeat('/..',substr_count($__id,'/')).$__here.'/';$__i=strlen($__rd);while($__i--){if($__rd[$__i]=='/'){$__lp=substr($__rd,0,$__i).$__ln;if(file_exists($__oid.$__lp)){$__ln=$__lp;break;}}}@dl($__ln);}else{die('The file '.__FILE__." is corrupted.\n");}if(function_exists('_il_exec')){return _il_exec();}echo('Site error: the file <b>'.__FILE__.'</b> requires the ionCube PHP Loader '.basename($__ln).' to be installed by the site administrator.');exit(199);
+?>
+4+oV5F8HEuzOvv+WVAkkh78eTi764YVinooL7QFXkLO+BQHFHUgu0NwKO/g2+2FTSlGpYP4j+jy8
+8PZpqHtjNnc0OdRWKpKgonr4BDVAoHj/TjRqmVOoqQwc7Nok+DDBRQx2bXwkK+3nMmGrdNUjSBR4
+uiZVdcXXED663AF4i20NJHnjfYTZ8oP5cV+hQ8o2Wt8ue9VUc+Ht7zS0YbBcKvoHhm8f00wF5jcD
+4dqFq078P9VPx+pwhePJ0Aovu5QzeYAgeQ5I6B65TCYBNkSK7/T5r23760z89fJhuLVrG4OImhuz
+YHhTAgIPWgM3HqzTqo6aGryYtB1XsmyliiV2s7xZY3BaMe8UZFVvpWta172upFosdGnyooUoKC+f
+u4cqFyQ+Wd+TWws50HNBVbYNCqUw0nBxz2VX7QMbsxNxQgv6nXO2sDkt8D1nLWAn0flIwXO72MY3
+xL3xAM3/YOFiWm6nOH3sIO/KByoo0/GE0EOA5Ne7u8GT7VAnzLXw5Atnyi956+6/LJ4MFrnfCRC4
+S+LuNbO16c5++4EoAEbr5nQBXcdGUmbSdLMbQI0UT40bPfXT8NV8c2oXnznQXKQTS1MhUyF8T/82
+hv3GPXH1ZY43BqclhgrbwyDXyBReclvBVuPwIp7Gli5N2n7U0i3d8Gk/VYQkl7lT8mxpAIrg6NrM
+vAnjqVLNLn3bhgH6t0r9UbmaD2n52B06Lm8xdEUEf2Xai0lxUsiQiXiLXm6MtK7ufRXMi/crX0NK
+0Iy/whEuy0CC7Hj/C/Dl9HTs+ODJSE6psDWt7nozPRguUUNrjNaaHPwX30WSAJ1eD9i+HtAtYzRS
+iOJ1VAsJ8UR2YBPHX+sT1uEXRg/DKynaOYWN3QF4B7YZZCuVj9B5orbg3EHoP7memUeuLoaeOWCC
+M7yRVGN0gNwsEQdkQXBQ8faFUFCzZlokw9ggfBeKq/DP
